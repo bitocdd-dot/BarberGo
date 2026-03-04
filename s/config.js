4275d5714b375a768163f09ba822cc5be6929881
bitocdd-dot <bitocdd@gmail.com>
2026-03-04 15:33:37 -0300

    Delete s directory

--- s/config.js
+++
@@ -1,5 +0,0 @@
-import { initializeApp } from "https://www.gstatic.com";
-import { getAuth } from "https://www.gstatic.com";
-const firebaseConfig = { apiKey: "AIzaSyB", authDomain: "barbergo.firebaseapp.com", projectId: "barbergo" };
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
