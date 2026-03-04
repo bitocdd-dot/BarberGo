fcba25a9af7f18bd4b5a849e719b472bdf8b8fd4
bitocdd-dot <bitocdd@gmail.com>
2026-03-04 14:53:33 -0300

    Add authentication functions for login and registration

---
+++ js/js/auth.js
@@ -0,0 +1,14 @@
+export function logar() {
+    const email = document.getElementById('email').value;
+    if(email) {
+        alert('Bem-vindo ao BarberGo!');
+        window.location.href = "mapa.html";
+    } else { 
+        alert('Por favor, preencha seu e-mail para acessar.'); 
+    }
+}
+
+export function cadastrar() {
+    alert('Conta criada com sucesso! Agora você pode fazer login.');
+    window.location.href = "login-cliente.html";
+}
