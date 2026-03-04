2fa517745119989d74ed4f122e177b969aefc347
bitocdd-dot <bitocdd@gmail.com>
2026-03-04 15:19:25 -0300

    Delete js directory

--- js/js/services.js
+++
@@ -1,9 +0,0 @@
-export const servicos = [
-    { id: 1, nome: "Corte Degradê", preco: 50.00 },
-    { id: 2, nome: "Barba Terapia", preco: 35.00 },
-    { id: 3, nome: "Combo Premium", preco: 80.00 }
-];
-
-export function calcularTotal(selecionados) {
-    return selecionados.reduce((total, item) => total + item.preco, 0);
-}
