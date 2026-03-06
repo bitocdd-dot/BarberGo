a07537be016084b521072152bac0bbdfe55ea9eb
bitocdd-dot <bitocdd@gmail.com>
2026-03-06 11:30:02 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-import Mapa from "./pages/Mapa";
+import React from 'react';
+import Mapa from './pages/Mapa';  // Ajusta se o caminho do Mapa for diferente
 
 function App() {
   return (
     <div>
+      <h1>BarberGo</h1>
       <Mapa />
     </div>
   );
