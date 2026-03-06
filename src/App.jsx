ecb0b812dbbf6a7910a34bd1ccec99c2442c3bb4
bitocdd-dot <bitocdd@gmail.com>
2026-03-06 03:21:18 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,8 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Mapa from "./pages/Mapa";
-import Perfil from "./pages/perfil";
-import Pagamento from "./pages/pagamento";
 
 function App() {
-
-  return (
-
-    <BrowserRouter>
-
-      <Routes>
-
-        <Route path="/" element={<Mapa />} />
-
-        <Route path="/perfil" element={<Perfil />} />
-
-        <Route path="/pagamento" element={<Pagamento />} />
-
-      </Routes>
-
-    </BrowserRouter>
-
-  );
-
+  return <Mapa />;
 }
 
 export default App;
