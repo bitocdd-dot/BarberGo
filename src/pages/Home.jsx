e49225b18fb9d0101f86b9db46c9995f3910ec4e
bitocdd-dot <bitocdd@gmail.com>
2026-03-07 03:21:55 -0300

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,49 @@
+export default function Home(){
+
+return(
+
+<div style={{
+height:"100vh",
+backgroundImage:"url('https://images.unsplash.com/photo-1503951914875-452162b0f3f1')",
+backgroundSize:"cover",
+backgroundPosition:"center",
+display:"flex",
+flexDirection:"column",
+justifyContent:"center",
+alignItems:"center",
+color:"#fff"
+}}>
+
+<h1 style={{
+fontSize:"48px",
+color:"#D4AF37",
+fontWeight:"bold",
+marginBottom:"40px"
+}}>
+BarberGo
+</h1>
+
+<button style={{
+background:"#D4AF37",
+color:"#000",
+border:"none",
+padding:"15px 40px",
+fontSize:"18px",
+borderRadius:"10px",
+fontWeight:"bold"
+}}>
+ENTRAR
+</button>
+
+<p style={{
+marginTop:"20px",
+fontSize:"14px"
+}}>
+Criar conta
+</p>
+
+</div>
+
+)
+
+}
