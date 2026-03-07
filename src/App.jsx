ccc1d748992db76e4aa8b27837b9dc1ce0e686dd
bitocdd-dot <bitocdd@gmail.com>
2026-03-07 03:50:01 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+import Home from "./pages/Home";
 import Mapa from "./pages/Mapa";
 
 function App() {
-  return <Mapa />;
+
+const [tela,setTela] = useState("home");
+
+if(tela === "home"){
+return <Home entrar={()=>setTela("mapa")} />;
+}
+
+if(tela === "mapa"){
+return <Mapa />;
+}
+
 }
 
 export default App;
