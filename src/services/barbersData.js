f107bd7cdd06fea58f0215bf5956849e19c85421
bitocdd-dot <bitocdd@gmail.com>
2026-03-07 00:13:43 -0300

    Update barbersData.js

--- src/services/barbersData.js
+++ src/services/barbersData.js
@@ -1,44 +1,21 @@
-const barbers = [
+// src/services/barbersData.js
+export const barbers = [
   {
     id: 1,
     name: "João Barbeiro",
-    photo: "https://images.unsplash.com/photo-1622253692010-333de7571860",
-    lat: -22.9718,
-    lng: -43.1868,
-    rating: 4.5,
-    services: ["Corte", "Barba", "Tintura"],
-    available: true
+    rating: 5,
+    services: ["Corte", "Barba"],
   },
   {
     id: 2,
     name: "Carlos Fade",
-    photo: "https://images.unsplash.com/photo-1611604548018-d56bba161d71",
-    lat: -22.9837,
-    lng: -43.2075,
-    rating: 4.8,
-    services: ["Corte Feminino", "Barba", "Hidratação"],
-    available: true
+    rating: 4,
+    services: ["Corte", "Barba", "Sobrancelha"],
   },
   {
     id: 3,
     name: "Marcos Style",
-    photo: "https://images.unsplash.com/photo-1583417319070-4e4615d91814",
-    lat: -22.9068,
-    lng: -43.1729,
-    rating: 4.8,
+    rating: 5,
     services: ["Corte", "Barba", "Sobrancelha"],
-    available: false
   },
-  {
-    id: 4,
-    name: "Lucas",
-    photo: "https://images.unsplash.com/photo-1622287162716-f311baa1a2b8",
-    lat: -22.7856,
-    lng: -43.3117,
-    rating: 4.7,
-    services: ["Corte Degradê", "Sobrancelha", "Barba"],
-    available: true
-  }
 ];
-
-export default barbers;
