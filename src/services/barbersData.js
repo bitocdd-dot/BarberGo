f5f9588293e45614f89fc6a7c501aad651fcf0b8
bitocdd-dot <bitocdd@gmail.com>
2026-03-07 01:33:59 -0300

    Update barbersData.js

--- src/services/barbersData.js
+++ src/services/barbersData.js
@@ -1,34 +1,31 @@
 const barbers = [
-{
-id: 1,
-name: "João Barbeiro",
-lat: -22.9718,
-lng: -43.1825,
-rating: 4.9,
-services: ["Corte", "Barba", "Sobrancelha"],
-price: 55,
-online: true
-},
-{
-id: 2,
-name: "Carlos Fade",
-lat: -22.9690,
-lng: -43.1860,
-rating: 4.7,
-services: ["Corte", "Barba"],
-price: 50,
-online: true
-},
-{
-id: 3,
-name: "Marcos Style",
-lat: -22.9735,
-lng: -43.1900,
-rating: 4.8,
-services: ["Corte", "Barba", "Pigmentação"],
-price: 65,
-online: true
-}
+  {
+    id: 1,
+    name: "João Barbeiro",
+    lat: -22.9718,
+    lng: -43.1825,
+    rating: 4.9,
+    photo: "https://i.imgur.com/6VBx3io.jpg",
+    online: true
+  },
+  {
+    id: 2,
+    name: "Carlos Fade",
+    lat: -22.9690,
+    lng: -43.1860,
+    rating: 4.7,
+    photo: "https://i.imgur.com/9R1F8yP.jpg",
+    online: true
+  },
+  {
+    id: 3,
+    name: "Marcos Style",
+    lat: -22.9735,
+    lng: -43.1900,
+    rating: 4.8,
+    photo: "https://i.imgur.com/FYb7J7S.jpg",
+    online: true
+  }
 ];
 
 export default barbers;
