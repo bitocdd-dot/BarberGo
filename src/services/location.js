9f038f8ed4ecac5a3a5bb33a6b9d22a0cbb0cc03
bitocdd-dot <bitocdd@gmail.com>
2026-03-07 01:32:52 -0300

    Update location.js

--- src/services/location.js
+++ src/services/location.js
@@ -1,5 +1,36 @@
-// src/services/location.js
-export const userLocation = {
-  lat: -22.911, 
-  lng: -43.209,
+let watchId = null;
+
+export const getCurrentLocation = () => {
+  return new Promise((resolve, reject) => {
+    if (!navigator.geolocation) {
+      reject("GPS não suportado");
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        resolve({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+      },
+      (err) => reject(err)
+    );
+  });
+};
+
+export const watchLocation = (callback) => {
+  if (!navigator.geolocation) return;
+
+  watchId = navigator.geolocation.watchPosition((pos) => {
+    callback({
+      lat: pos.coords.latitude,
+      lng: pos.coords.longitude,
+    });
+  });
+};
+
+export const stopWatching = () => {
+  if (watchId) {
+    navigator.geolocation.clearWatch(watchId);
+  }
 };
