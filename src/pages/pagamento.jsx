1f01f183c072e4aae0d4a9effb2ff5b50aef7fa5
bitocdd-dot <bitocdd@gmail.com>
2026-03-07 05:25:56 -0300

    Create pagamento.jsx

---
+++ src/pages/pagamento.jsx
@@ -0,0 +1,10 @@
+function Pagamento() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Pagamento BarberGo</h1>
+      <p>Pagamento do barbeiro aparecerá aqui.</p>
+    </div>
+  );
+}
+
+export default Pagamento;
