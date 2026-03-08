99cc5d9ba5b72421ffc9e7a4224320e15e1cb7ab
bitocdd-dot <bitocdd@gmail.com>
2026-03-08 10:11:20 -0300

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,1 @@
-import { createClient } from "@supabase/supabase-js"
-
-const supabaseUrl = "https://tvbkajyrdmpdohdupexz.supabase.co"
-
-const supabaseKey = "sb_publishable_Xv-ys__zqYsubEuEYyNgcg_fDV8h8Lf"
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = null;
