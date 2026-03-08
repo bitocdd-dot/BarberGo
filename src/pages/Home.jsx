be18ba1bcede0a2520d08d8af22a22a2c75a26c0
bitocdd-dot <bitocdd@gmail.com>
2026-03-07 22:45:50 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
-import CadastroBarbeiro from "./cadastroBarbeiro";
-import Mapa from "./Mapa";
+import CadastroBarbeiro from "./cadastroBarbeiro"; // deve estar na mesma pasta
+import Mapa from "./Mapa"; // deve estar na mesma pasta
+import "../node_modules/leaflet/dist/leaflet.css"; // garante que o mapa funciona
 
 export default function Home() {
   return (
-    <div>
+    <div style={{ padding: "10px", textAlign: "center" }}>
       <h1>BarberGo</h1>
       <CadastroBarbeiro />
       <Mapa />
