ec0817825e16e53142481aa3a2e0cc3b63fc3094
bitocdd-dot <bitocdd@gmail.com>
2026-03-08 10:49:29 -0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://SEU_PROJETO.supabase.co";
-const supabaseKey = "SUA_ANON_KEY";
+const supabaseUrl = "COLE_AQUI_SUA_URL_DO_SUPABASE";
+const supabaseKey = "COLE_AQUI_SUA_ANON_KEY";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
