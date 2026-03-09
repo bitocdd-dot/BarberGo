fe716156a90868e25881d6eaafad7e91b70414f7
bitocdd-dot <bitocdd@gmail.com>
2026-03-09 15:28:16 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,19 @@
+import CadastroBarbeiro from "./CadastroBarbeiro.jsx";
+import Mapa from "./Mapa.jsx";
+import "../index.css";
 
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>BarberGo</h1>
+
+      {/* Formulário de cadastro */}
+      <CadastroBarbeiro />
+
+      {/* Mapa */}
+      <div style={{ marginTop: "20px" }}>
+        <Mapa />
+      </div>
+    </div>
+  );
+}
