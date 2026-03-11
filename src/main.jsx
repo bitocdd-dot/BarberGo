cd33d802dab07071f642d83db58b4d5a4f1d5b7f
bitocdd-dot <bitocdd@gmail.com>
2026-03-10 23:29:39 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// IMPORTS CORRIGIDOS
 import Home from "./pages/Home.jsx";
 import CadastroBarbeiro from "./pages/CadastroBarbeiro.jsx";
 import Mapa from "./pages/Mapa.jsx";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
