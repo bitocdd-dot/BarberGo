31ec74b947c00023948235cb8bee50f93a100225
bitocdd-dot <bitocdd@gmail.com>
2026-03-14 03:38:16 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,19 +12,12 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-
       <Routes>
-
         <Route path="/" element={<Home />} />
-
         <Route path="/cadastrobarbeiro" element={<CadastroBarbeiro />} />
-
         <Route path="/mapa" element={<Mapa />} />
-
-        <Route path="/perfilbarbeiro/:id" element={<PerfilBarbeiro />} />
-
+        <Route path="/perfilbarbeiro" element={<PerfilBarbeiro />} />
       </Routes>
-
     </BrowserRouter>
   </React.StrictMode>
 );
