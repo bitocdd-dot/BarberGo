dd9925215f0fc4982e9efc6dc19b5cbc197fdd9f
bitocdd-dot <bitocdd@gmail.com>
2026-03-13 23:29:03 -0300

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,1 +1,20 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>BarberGo</h1>
 
+      <p>Bem-vindo! Escolha uma opção:</p>
+
+      <a href="/cadastrobarbeiro">
+        <button>Cadastrar Barbeiro</button>
+      </a>
+
+      <br />
+      <br />
+
+      <a href="/mapa">
+        <button>Ver Mapa</button>
+      </a>
+    </div>
+  );
+}
