dfa7f3a55d5943b60b1837415f20c2796b3ee19b
bitocdd-dot <bitocdd@gmail.com>
2026-03-19 20:44:08 -0300

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,10 +1,8 @@
+// src/services/supabase.js
 import { createClient } from '@supabase/supabase-js';
 
-// URL do seu projeto Supabase
-const supabaseUrl = 'https://tvbkajyrdmpdohdupexz.supabase.co';
+// Conexão usando variáveis de ambiente do Vite
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// Chave anônima (anon key)
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR2YmthanlyZG1wZG9oZHVwZXh6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4NTYxNzMsImV4cCI6MjA4ODQzMjE3M30.q2hO148dpFWcMSwkYRSkA_wOwIPgElE2c-uz4hw_S_I';
-
-// Cliente Supabase
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
