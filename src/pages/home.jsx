a5a4f4f76d9dfcac80bf0efb4e8b23e34064638e
bitocdd-dot <bitocdd@gmail.com>
2026-03-19 21:22:34 -0300

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,18 +4,15 @@ import '../index.css';
 
 export default function Home() {
   return (
-    <div style={{ textAlign: 'center', padding: '50px' }}>
-      <h1>Bem-vindo ao BarberGo</h1>
-      <p>Escolha sua opção abaixo:</p>
-      <div style={{ marginTop: '20px' }}>
+    <div className="home-container" style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Bem-vindo ao BarberGo!</h1>
+      <p>Escolha uma opção:</p>
+      <div style={{ marginTop: '30px' }}>
         <Link to="/login">
-          <button>Login</button>
+          <button style={{ marginRight: '10px' }}>Login</button>
         </Link>
-        <Link to="/cadastrobarbeiro" style={{ marginLeft: '10px' }}>
+        <Link to="/cadastrobarbeiro">
           <button>Cadastro Barbeiro</button>
-        </Link>
-        <Link to="/mapa" style={{ marginLeft: '10px' }}>
-          <button>Mapa</button>
         </Link>
       </div>
     </div>
