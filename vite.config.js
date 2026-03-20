4bac704ae5c3fe489567ae8cf732679f2c1add27
bitocdd-dot <bitocdd@gmail.com>
2026-03-20 04:30:57 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: 'src',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true
-  },
   plugins: [react()],
-});
+  root: '.',          // garante que o index.html da raiz seja usado
+  build: {
+    outDir: 'dist'
+  }
+})
