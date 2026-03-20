a3e253755d8121a06111db63b2d9f4a9c6990f7b
bitocdd-dot <bitocdd@gmail.com>
2026-03-19 23:41:40 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/home.jsx';
+import Login from './pages/login.jsx';
+import CadastroBarbeiro from './pages/cadastrobarbeiro.jsx';
+import PerfilBarbeiro from './pages/perfilbarbeiro.jsx';
+import Mapa from './pages/mapa.jsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/cadastrobarbeiro" element={<CadastroBarbeiro />} />
+      <Route path="/perfilbarbeiro" element={<PerfilBarbeiro />} />
+      <Route path="/mapa" element={<Mapa />} />
+    </Routes>
+  </BrowserRouter>
+);
