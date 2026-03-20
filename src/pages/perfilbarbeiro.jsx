9ff8ab2f0e54750a86d70c7dc66aa4256deba754
bitocdd-dot <bitocdd@gmail.com>
2026-03-19 22:25:26 -0300

    Update perfilbarbeiro.jsx

--- src/pages/perfilbarbeiro.jsx
+++ src/pages/perfilbarbeiro.jsx
@@ -3,9 +3,17 @@ import '../index.css';
 
 export default function PerfilBarbeiro() {
   return (
-    <div className="perfil-container">
+    <div className="perfil-container" style={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      backgroundColor: '#fafafa',
+      color: '#333'
+    }}>
       <h1>Perfil do Barbeiro</h1>
-      <p>Detalhes do barbeiro aparecerão aqui.</p>
+      <p>Aqui aparecerão os detalhes do barbeiro</p>
     </div>
   );
 }
