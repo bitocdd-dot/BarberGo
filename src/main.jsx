e2b1db6a455c8fa15cab5383132b5bf01befaf8e
bitocdd-dot <bitocdd@gmail.com>
2026-03-24 08:47:53 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import Home from './pages/home.jsx';
-import Login from './pages/login.jsx';
-import CadastroBarbeiro from './pages/cadastrobarbeiro.jsx';
-import PerfilBarbeiro from './pages/perfilbarbeiro.jsx';
-import Mapa from './pages/mapa.jsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/cadastrobarbeiro" element={<CadastroBarbeiro />} />
-      <Route path="/perfilbarbeiro" element={<PerfilBarbeiro />} />
-      <Route path="/mapa" element={<Mapa />} />
-    </Routes>
-  </BrowserRouter>
-);
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
