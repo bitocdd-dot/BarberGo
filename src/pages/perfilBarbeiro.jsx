30fd5c64d2d0f7d38a2d2fc7cc6149e5aa6e4ef3
bitocdd-dot <bitocdd@gmail.com>
2026-03-24 23:38:20 -0300

    Update perfilBarbeiro.jsx

--- src/pages/perfilBarbeiro.jsx
+++ src/pages/perfilBarbeiro.jsx
@@ -1,1 +1,28 @@
+import "./perfilBarbeiro.css";
+import { useLocation } from "react-router-dom";
 
+export default function PerfilBarbeiro() {
+  const { state } = useLocation();
+  const barbeiro = state;
+
+  return (
+    <div className="perfil-container">
+      <img src={barbeiro.foto} className="perfil-foto" />
+
+      <h2>{barbeiro.nome}</h2>
+      <p className="avaliacao">⭐ {barbeiro.avaliacao}</p>
+
+      <h3>Especialidades</h3>
+      <p>{barbeiro.especialidades}</p>
+
+      <h3>Portfólio</h3>
+      <div className="portfolio-box">
+        <p>O barbeiro ainda não adicionou fotos.</p>
+      </div>
+
+      <button className="btn-agendar">Agendar</button>
+
+      <button className="btn-domicilio">Domicílio</button>
+    </div>
+  );
+}
