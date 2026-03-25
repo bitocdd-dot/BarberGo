6fc6054e881614354093baa83dd306e05b2526ee
bitocdd-dot <bitocdd@gmail.com>
2026-03-25 01:38:05 -0300

    Create header.jsx

---
+++ src/components/header.jsx
@@ -0,0 +1,7 @@
+export default function Header({ title }) {
+  return (
+    <div className="w-full py-4 text-center bg-black text-white font-bold text-lg shadow">
+      {title}
+    </div>
+  );
+}
