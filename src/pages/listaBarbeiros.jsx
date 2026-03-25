bd90f62dd385972e07f184319f183a665605d036
bitocdd-dot <bitocdd@gmail.com>
2026-03-25 00:08:58 -0300

    Update listaBarbeiros.jsx

--- src/pages/listaBarbeiros.jsx
+++ src/pages/listaBarbeiros.jsx
@@ -1,44 +1,56 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../../services/supabase";
 import "./listaBarbeiros.css";
 import { useNavigate } from "react-router-dom";
 
-const barbeiros = [
-  {
-    id: 1,
-    nome: "Carlos Barber",
-    avaliacao: 4.9,
-    especialidades: "Corte Fade, Barba, Sobrancelha",
-    foto: "/src/assets/4F0D9D57-C2FE-4F94-B3DB-2123C22AB545.png",
-  },
-  {
-    id: 2,
-    nome: "João Style",
-    avaliacao: 4.7,
-    especialidades: "Tesoura, Moicano, Militar",
-    foto: "/src/assets/4F0D9D57-C2FE-4F94-B3DB-2123C22AB545.png",
-  },
-];
-
 export default function ListaBarbeiros() {
+  const [barbers, setBarbers] = useState([]);
   const navigate = useNavigate();
 
-  const verPerfil = (barbeiro) => {
-    navigate("/perfilBarbeiro", { state: barbeiro });
-  };
+  useEffect(() => {
+    loadBarbers();
+  }, []);
+
+  async function loadBarbers() {
+    const { data, error } = await supabase.from("barbers").select("*");
+
+    if (error) {
+      console.error("Erro ao carregar barbeiros:", error);
+      return;
+    }
+
+    setBarbers(data);
+  }
+
+  function abrirPerfil(id) {
+    navigate(`/perfilBarbeiro?id=${id}`);
+  }
 
   return (
     <div className="lista-container">
       <h2 className="titulo-lista">Barbeiros Disponíveis</h2>
 
-      {barbeiros.map((b) => (
-        <div key={b.id} className="card-barbeiro">
-          <img src={b.foto} className="foto-barbeiro" />
+      {barbers.map((barber) => (
+        <div className="barber-card" key={barber.id}>
+          <img
+            src={
+              barber.profile_image && barber.profile_image !== "EMPTY"
+                ? barber.profile_image
+                : "/placeholder.jpg"
+            }
+            alt="Foto"
+            className="barber-foto"
+          />
 
-          <div className="info-barbeiro">
-            <h3>{b.nome}</h3>
-            <p>⭐ {b.avaliacao}</p>
-            <p>{b.especialidades}</p>
+          <div className="barber-info">
+            <h3>{barber.name}</h3>
+            <p className="rating">⭐ {barber.rating || 5}</p>
+            <p className="email">{barber.email}</p>
 
-            <button onClick={() => verPerfil(b)} className="btn-ver">
+            <button
+              onClick={() => abrirPerfil(barber.id)}
+              className="btn-ver-perfil"
+            >
               Ver Perfil
             </button>
           </div>
